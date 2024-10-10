a7c2db3d171d4532f6668f33d1809feaf2afda09
Mitzimberg <BlakeMitzimberg003@gmail.com>
2024-10-10 11:23:46 -0500

    work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-// JS scripts placed here
+function showTab(tabId) {
+    // I've got no real idea what any of this means, all of this was jacked
+    const tabItems = document.querySelectorAll('.tab-item');
+    tabItems.forEach(item => {
+        item.classList.remove('active');
+    });
+
+    // Remove active class from all tabs
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    document.getElementById(tabId).classList.add('active');
+    document.querySelector(`.tab[onclick*="${tabId}"]`).classList.add('active');
+}
+
+showTab('tab1');
